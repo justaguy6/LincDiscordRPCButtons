6c50d05a82f6b7617458ea6c13afb074555f45d7
Random people <98900217+justaguy6@users.noreply.github.com>
2023-04-24 09:59:53 -0300

    Update linc_discord_rpc.h

--- linc/linc_discord_rpc.h
+++ linc/linc_discord_rpc.h
@@ -1,31 +1,67 @@
-#pragma once
-#ifdef _WIN32
-    #pragma comment(lib, "advapi32")
-#endif
-
-#include <hxcpp.h>
+#include <jni.h>
+#include <string>
+#include <android/log.h>
 #include "../lib/discord-rpc/include/discord-rpc.h"
 
-namespace linc
-{
-    namespace discord_rpc
-    {
-        extern void init(const char* _clientID, const char* _steamAppID,
-            void (*_onReady)(),
-            void (*_onDisconnected)(int errorCode, const char* message),
-            void (*_onError)(int errorCode, const char* message),
-            void (*_onJoin)(const char* joinSecret),
-            void (*_onSpectate)(const char* spectateSecret),
-            void (*_onRequest)(const DiscordJoinRequest* request));
-        extern void process();
-        extern void update_presence(
-            const char* _state, const char* _details,
-            int64_t _startTimestamp, int64_t _endTimestamp,
-            const char* _largeImageKey, const char* _largeImageText,
-            const char* _smallImageKey, const char* _smallImageText,
-            const char* _partyID, int _partySize, int _partyMax, const char* _matchSecret, const char* _joinSecret, const char* _spectateSecret,
-            int8_t _instance, const char* button1Text, const char* button1Url, const char* button2Text, const char* button2Url);
-        extern void respond(const char* _userID, int _reply);
-        extern void shutdown();
+extern "C" {
+
+    void onReadyCallback() {
+     
     }
-}
+
+    void onDisconnectedCallback(int errorCode, const char* message) {
+       
+    }
+
+    void onErrorCallback(int errorCode, const char* message) {
+        
+    }
+
+    void onJoinCallback(const char* joinSecret) {
+       
+    }
+
+    void onSpectateCallback(const char* spectateSecret) {
+      
+    }
+
+    void onRequestCallback(const DiscordJoinRequest* request) {
+        
+    }
+
+    JNIEXPORT void JNICALL Java_com_example_myapplication_DiscordRpc_init(JNIEnv* env, jobject /* this */, jstring clientID, jstring steamAppID) {
+        const char* _clientID = env->GetStringUTFChars(clientID, 0);
+        const char* _steamAppID = env->GetStringUTFChars(steamAppID, 0);
+
+        ::DiscordEventHandlers handlers{};
+        memset(&handlers, 0, sizeof(handlers));
+        handlers.ready = onReadyCallback;
+        handlers.disconnected = onDisconnectedCallback;
+        handlers.errored = onErrorCallback;
+        handlers.joinGame = onJoinCallback;
+        handlers.spectateGame = onSpectateCallback;
+        handlers.joinRequest = onRequestCallback;
+        ::Discord_Initialize(_clientID, &handlers, 1, _steamAppID);
+
+        env->ReleaseStringUTFChars(clientID, _clientID);
+        env->ReleaseStringUTFChars(steamAppID, _steamAppID);
+    }
+
+    JNIEXPORT void JNICALL Java_com_example_myapplication_DiscordRpc_process(JNIEnv* env, jobject /* this */) {
+        
+        ::Discord_RunCallbacks();
+    }
+
+    JNIEXPORT void JNICALL Java_com_example_myapplication_DiscordRpc_updatePresence(JNIEnv* env, jobject /* this */,
+        jstring state, jstring details, jlong startTimestamp, jlong endTimestamp,
+        jstring largeImageKey, jstring largeImageText,
+        jstring smallImageKey, jstring smallImageText,
+        jstring partyID, jint partySize, jint partyMax, jstring matchSecret, jstring joinSecret, jstring spectateSecret,
+        jbyte instance, jstring button1Text, jstring button1Url, jstring button2Text, jstring button2Url) {
+        const char* _state = env->GetStringUTFChars(state, 0);
+        const char* _details = env->GetStringUTFChars(details, 0);
+        const char* _largeImageKey = env->GetStringUTFChars(largeImageKey, 0);
+        const char* _largeImageText = env->GetStringUTFChars(largeImageText, 0);
+        const char* _smallImageKey = env->GetStringUTFChars(smallImageKey, 0);
+        const char* _smallImageText = env->GetStringUTFChars(smallImageText, 0);
+       
